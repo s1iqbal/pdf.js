e7399020cce0610bd542b794e83ca4ec7bb5b95d
Saad Iqbal <s1iqbal@ryerson.ca>
2018-02-05 20:50:31 -0500

    Fixes {8851}

--- test/unit/network_spec.js
+++ test/unit/network_spec.js
@@ -63,16 +63,30 @@ describe('network', function() {
   });
 
   it('read with streaming', function(done) {
+
     var userAgent = window.navigator.userAgent;
     // The test is valid for FF only: the XHR has support of the
-    // 'moz-chunked-array' response type.
+    // 'moz-chunked-arraybuffer' response type.
     // TODO enable for other browsers, e.g. when fetch/streams API is supported.
-    var m = /Mozilla\/5.0.*?rv:(\d+).*? Gecko/.exec(userAgent);
-    if (!m || m[1] < 9) {
-      expect(true).toEqual(true);
-      done();
-      return;
+    var isFirefoxWithMozChunkedEncodingSupport = /Mozilla\/5.0.*?rv:(\d+).*? Gecko/.exec(userAgent);
+    var isFetchWithStreamSupport = typeof ReadableStream;
+
+    let isFetchWithStreamSupport = (typeof Response !== 'undefined' &&
+                                'body' in Response.prototype &&
+                                typeof ReadableStream !== 'undefined');
+    if (!isFirefoxWithMozChunkedEncodingSupport && !isFetchWithStreamSupport) {
+      pending('Streaming not supported by user agent');
     }
+
+
+
+
+
+
+
+
+
+
 
     var stream = new PDFNetworkStream({
       url: pdf2,
